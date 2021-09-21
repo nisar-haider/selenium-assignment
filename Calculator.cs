cb0670ba553922b46a7c9a85b80e10a3df4667bc
DESKTOP-4483D1F\Nisar <nisar.haider454@gmail.com>
2021-09-21 13:50:37 +0500

    Added wait functions in all the test cases of Calculator Suite

--- Calculator.cs
+++ Calculator.cs
@@ -42,20 +42,48 @@ namespace selenium_assignment
 
         }
 
+        void waitFortheElementToDisplayed(WindowsDriver<WindowsElement> Session,string name){
+            bool value=false;
+            for(int i=0 ;i<90 ;i++)
+            {
+                try{
+                    value=Session.FindElementByName(name).Displayed;
+                }
+                catch(Exception) 
+                {
+                    value=false;
+                }
+                if(value){
+                    break;
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
         [Test]
         public void calculatorAddition()
         {
             CreateWinAppSession();
-        
+
+            waitFortheElementToDisplayed(CalculatorSession,"One");
             CalculatorSession.FindElementByName("One").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Plus");
             CalculatorSession.FindElementByName("Plus").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Five");
             CalculatorSession.FindElementByName("Five").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Equals");
             CalculatorSession.FindElementByName("Equals").Click();
 
             String result = CalculatorSession.FindElementByAccessibilityId("CalculatorResults").Text;
 
             Assert.AreEqual(result,"Display is 6");
 
+            waitFortheElementToDisplayed(CalculatorSession,"Close Calculator");
             CalculatorSession.FindElementByName("Close Calculator").Click();
 
         }
@@ -64,15 +92,24 @@ namespace selenium_assignment
         public void calculatorSubtraction()
         {
             CreateWinAppSession();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Five");
             CalculatorSession.FindElementByName("Five").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Minus");
             CalculatorSession.FindElementByName("Minus").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Two");
             CalculatorSession.FindElementByName("Two").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Equals");
             CalculatorSession.FindElementByName("Equals").Click();
 
             String result = CalculatorSession.FindElementByAccessibilityId("CalculatorResults").Text;
 
             Assert.AreEqual(result,"Display is 3");
 
+            waitFortheElementToDisplayed(CalculatorSession,"Close Calculator");
             CalculatorSession.FindElementByName("Close Calculator").Click();
 
         }
@@ -81,16 +118,24 @@ namespace selenium_assignment
         public void calculatorDivision()
         {
             CreateWinAppSession();
-        
+
+            waitFortheElementToDisplayed(CalculatorSession,"Nine");
             CalculatorSession.FindElementByName("Nine").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Divide by");
             CalculatorSession.FindElementByName("Divide by").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Three");
             CalculatorSession.FindElementByName("Three").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Equals");
             CalculatorSession.FindElementByName("Equals").Click();
 
             String result = CalculatorSession.FindElementByAccessibilityId("CalculatorResults").Text;
 
             Assert.AreEqual(result,"Display is 3"); 
 
+            waitFortheElementToDisplayed(CalculatorSession,"Close Calculator");
             CalculatorSession.FindElementByName("Close Calculator").Click();
 
         }
@@ -100,15 +145,23 @@ namespace selenium_assignment
         {
             CreateWinAppSession();
 
+            waitFortheElementToDisplayed(CalculatorSession,"Three");
             CalculatorSession.FindElementByName("Three").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Multiply by");
             CalculatorSession.FindElementByName("Multiply by").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Three");
             CalculatorSession.FindElementByName("Three").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Equals");
             CalculatorSession.FindElementByName("Equals").Click();
 
             String result = CalculatorSession.FindElementByAccessibilityId("CalculatorResults").Text;
 
             Assert.AreEqual(result,"Display is 9"); 
 
+            waitFortheElementToDisplayed(CalculatorSession,"Close Calculator");
             CalculatorSession.FindElementByName("Close Calculator").Click();
 
         }
@@ -118,19 +171,26 @@ namespace selenium_assignment
         {
             CreateWinAppSession();
 
+            waitFortheElementToDisplayed(CalculatorSession,"Three");
             CalculatorSession.FindElementByName("Three").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Square");
             CalculatorSession.FindElementByName("Square").Click();
             String result = CalculatorSession.FindElementByAccessibilityId("CalculatorResults").Text;
 
             Assert.AreEqual(result,"Display is 9"); 
 
+            waitFortheElementToDisplayed(CalculatorSession,"Five");
             CalculatorSession.FindElementByName("Five").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Square");
             CalculatorSession.FindElementByName("Square").Click();
             result = CalculatorSession.FindElementByAccessibilityId("CalculatorResults").Text;
         
 
             Assert.AreEqual(result,"Display is 25"); 
 
+            waitFortheElementToDisplayed(CalculatorSession,"Close Calculator");
             CalculatorSession.FindElementByName("Close Calculator").Click();
 
         }
@@ -139,17 +199,27 @@ namespace selenium_assignment
         public void calculatorClearScreen()
         {
             CreateWinAppSession();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Three");
             CalculatorSession.FindElementByName("Three").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Multiply by");
             CalculatorSession.FindElementByName("Multiply by").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Three");
             CalculatorSession.FindElementByName("Three").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Equals");
             CalculatorSession.FindElementByName("Equals").Click();
 
+            waitFortheElementToDisplayed(CalculatorSession,"Clear");
             CalculatorSession.FindElementByName("Clear").Click();
 
             String result = CalculatorSession.FindElementByAccessibilityId("CalculatorResults").Text;
 
             Assert.AreEqual(result,"Display is 0"); 
 
+            waitFortheElementToDisplayed(CalculatorSession,"Close Calculator");
             CalculatorSession.FindElementByName("Close Calculator").Click();
 
         }
@@ -159,7 +229,11 @@ namespace selenium_assignment
         public void calculatorOpenScientificMode()
         {
             CreateWinAppSession();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Open Navigation");
             CalculatorSession.FindElementByName("Open Navigation").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Scientific Calculator");
             CalculatorSession.FindElementByName("Scientific Calculator").Click();
 
             bool displayed = CalculatorSession.FindElementByName("Scientific Calculator mode").Displayed;
@@ -167,9 +241,16 @@ namespace selenium_assignment
             Assert.IsTrue(displayed); 
 
             //Check Modulo function
+            waitFortheElementToDisplayed(CalculatorSession,"Nine");
             CalculatorSession.FindElementByName("Nine").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Modulo");
             CalculatorSession.FindElementByName("Modulo").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Three");
             CalculatorSession.FindElementByName("Three").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Equals");
             CalculatorSession.FindElementByName("Equals").Click();
 
             String result = CalculatorSession.FindElementByAccessibilityId("CalculatorResults").Text;
@@ -177,11 +258,19 @@ namespace selenium_assignment
 
             Assert.AreEqual(result,"Display is 0"); 
 
+            waitFortheElementToDisplayed(CalculatorSession,"Clear entry");
             CalculatorSession.FindElementByName("Clear entry").Click();
 
+            waitFortheElementToDisplayed(CalculatorSession,"Seven");
             CalculatorSession.FindElementByName("Seven").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Modulo");
             CalculatorSession.FindElementByName("Modulo").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Three");
             CalculatorSession.FindElementByName("Three").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Equals");
             CalculatorSession.FindElementByName("Equals").Click();
 
             result = CalculatorSession.FindElementByAccessibilityId("CalculatorResults").Text;
@@ -189,24 +278,43 @@ namespace selenium_assignment
 
             Assert.AreEqual(result,"Display is 1");
 
+            waitFortheElementToDisplayed(CalculatorSession,"Clear entry");
             CalculatorSession.FindElementByName("Clear entry").Click();
 
             //Check exponential function
+            waitFortheElementToDisplayed(CalculatorSession,"Five");
             CalculatorSession.FindElementByName("Five").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Exponential");
             CalculatorSession.FindElementByName("Exponential").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Two");
             CalculatorSession.FindElementByName("Two").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Equals");
             CalculatorSession.FindElementByName("Equals").Click();
 
             result = CalculatorSession.FindElementByAccessibilityId("CalculatorResults").Text;
         
 
             Assert.AreEqual(result,"Display is 2");
+
+            waitFortheElementToDisplayed(CalculatorSession,"Clear entry");
             CalculatorSession.FindElementByName("Clear entry").Click();
             //Calculating Sin function
+            waitFortheElementToDisplayed(CalculatorSession,"Nine");
             CalculatorSession.FindElementByName("Nine").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Zero");
             CalculatorSession.FindElementByName("Zero").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Trigonometry");
             CalculatorSession.FindElementByName("Trigonometry").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Sine");
             CalculatorSession.FindElementByName("Sine").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Equals");
             CalculatorSession.FindElementByName("Equals").Click();
 
             result = CalculatorSession.FindElementByAccessibilityId("CalculatorResults").Text;
@@ -214,15 +322,20 @@ namespace selenium_assignment
 
             Assert.AreEqual(result,"Display is 1");
 
+            waitFortheElementToDisplayed(CalculatorSession,"Clear entry");
             CalculatorSession.FindElementByName("Clear entry").Click();
 
+            waitFortheElementToDisplayed(CalculatorSession,"Open Navigation");
             CalculatorSession.FindElementByName("Open Navigation").Click();
+
+            waitFortheElementToDisplayed(CalculatorSession,"Standard Calculator");
             CalculatorSession.FindElementByName("Standard Calculator").Click();
 
             bool displayed1 = CalculatorSession.FindElementByName("Standard Calculator mode").Displayed;
 
             Assert.IsTrue(displayed);
 
+            waitFortheElementToDisplayed(CalculatorSession,"Close Calculator");
             CalculatorSession.FindElementByName("Close Calculator").Click();
 
         }
