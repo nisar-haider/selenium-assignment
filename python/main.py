a32f595ff20b3ba3f3831b736b7247cc1f706ee1
DESKTOP-4483D1F\Nisar <nisar.haider454@gmail.com>
2021-09-29 20:31:12 +0500

    This commit contains the assertions and explicit waits in the test cases

--- python/main.py
+++ python/main.py
@@ -1,56 +1,83 @@
+from _ast import Assert
+
+import self as self
 from selenium import webdriver
 import time
+import unittest
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common import by
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from Pages.TTHomePage import TTHomePage
 from Pages.Google import GooglePage
 from Pages.Portfolio import Portfolio
 
-driver = webdriver.Chrome()
-driver.maximize_window()
+class Tintash(unittest.TestCase):
 
-driver.get("https://www.google.com/")
-# driver.switch_to_frame(driver.find_element_by_xpath("//iframe[@class='demo-frame']"))
-#
-# source = driver.find_element_by_xpath("//*[text()='Drag me to my target']")
-# target = driver.find_element_by_xpath("//*[text()='Drop here']")
-#
-# actions = ActionChains(driver)
-# actions.drag_and_drop(source, target).perform()
+    @classmethod
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("https://www.google.com/")
 
-googlepage = GooglePage(driver)
-ttHome = TTHomePage(driver)
-portfolio = Portfolio(driver)
-
-googlepage.searchInput("Tintash")
-googlepage.clickSearchButton()
-driver.implicitly_wait(5)
-googlepage.clickTintashLink()
-
-ttHome.clickPortfolio()
-
-time.sleep(2)
-#portfolio.displayTxtPortfolio()
-
-Element = driver.find_element_by_xpath("//span[text()='View Casestudy']")
-driver.execute_script("arguments[0].scrollIntoView();", Element)
-
-#portfolio.displaydd_Solution()
-portfolio.clickdd_Solution()
-portfolio.clickchkbox_ddL_Solution()
-
-#portfolio.displaydd_Technology()
-portfolio.clickdd_Technology()
-portfolio.clickchkbox_ddL_Technology()
-
-#portfolio.displaydd_Industry()
-portfolio.clickdd_Industry()
-
-Element1 = driver.find_element_by_xpath("//*[text()='Retail & eCommerce']")
-driver.execute_script("arguments[0].scrollIntoView();", Element1)
-portfolio.clickchkbox_ddL_Industry()
+    @classmethod
+    def test_TintashWebsite(self):
+        wait = WebDriverWait(self.driver, 60)
+        googlepage = GooglePage(self.driver)
+        ttHome = TTHomePage(self.driver)
+        portfolio = Portfolio(self.driver)
+        googlepage.searchInput("Tintash")
+        try:
+            wait.until(EC.presence_of_element_located((By.XPATH, googlepage.btn_search)))
+            print ("Element is ready")
+            googlepage.clickSearchButton()
+        except TimeoutException:
+            print ("Element is not loaded")
 
 
+        wait.until(EC.element_to_be_clickable((By.XPATH, googlepage.tintashLink)))
+        googlepage.clickTintashLink()
+
+        wait.until(EC.presence_of_element_located((By.XPATH, ttHome.btn_Portfolio)))
+        ttHome.clickPortfolio()
+
+        time.sleep(2)
+
+        Element = self.driver.find_element_by_xpath("//span[text()='View Casestudy']")
+        self.driver.execute_script("arguments[0].scrollIntoView();", Element)
+
+        #portfolio.displaydd_Solution()
+        wait.until(EC.presence_of_element_located((By.XPATH, portfolio.dd_Solution)))
+        portfolio.clickdd_Solution()
+        portfolio.clickchkbox_ddL_Solution()
+
+        #portfolio.displaydd_Technology()
+        wait.until(EC.presence_of_element_located((By.XPATH, portfolio.dd_Technology)))
+        portfolio.clickdd_Technology()
+        portfolio.clickchkbox_ddL_Technology()
+
+        #portfolio.displaydd_Industry()
+        wait.until(EC.presence_of_element_located((By.XPATH, portfolio.dd_Industry)))
+        portfolio.clickdd_Industry()
+
+        Element1 = self.driver.find_element_by_xpath("//*[text()='Retail & eCommerce']")
+        self.driver.execute_script("arguments[0].scrollIntoView();", Element1)
+        portfolio.clickchkbox_ddL_Industry()
+
+        time.sleep(5)
+        Element2 = self.driver.find_element_by_xpath("//*[contains(text(),'Camaradly is an employee-first')]")
+        self.driver.execute_script("arguments[0].scrollIntoView();", Element2)
+
+        time.sleep(5)
+
+        wait.until(EC._element_if_visible((By.XPATH, portfolio.txt_Camaradly)))
+        #elem = self.driver.find_element_by_xpath(portfolio.txt_Camaradly)
+        #self.assertTrue(elem).is_displayed()
+        print("Camaradly exists!")
 
 
-
-time.sleep(5)
+    time.sleep(5)
+    def tearDown(self):
+        self.driver.quit()
