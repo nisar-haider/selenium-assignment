7a837a90c6e4394aea1ca39e498d3f6c2ec3557c
DESKTOP-4483D1F\Nisar <nisar.haider454@gmail.com>
2021-10-08 18:53:46 +0500

    File: config.py What:Get name of the test class for generic functions Description: I am able to get the name of the test cases class for the generic use in reporting

--- python/Configurations/config.py
+++ python/Configurations/config.py
@@ -2,17 +2,40 @@ from selenium import webdriver
 import time
 import unittest
 import HtmlTestRunner
-#
-class config(unittest.TestCase):
+from HtmlTestRunner import HTMLTestRunner
+
+from unittest import TestLoader, TestSuite
+
+#import tintash
+#import webAutomation
+#import tintash
+
+
+class config1(unittest.TestCase):
+
 
     def setUp(self):
         self.driver = webdriver.Chrome("C:/Users/Duraze/PycharmProjects/pythonProject/chromedriver.exe")
         self.driver.maximize_window()
+        print(self.id())
+        smoke = self.id()
+        smoke1 = type(self).__name__
+        print(smoke)
+        smoke_test=unittest.TestSuite
+        smoke_test.addTests([unittest.defaultTestLoader.loadTestsFromTestCase(smoke1)])
+        #smoke_test.a
+        runner = HTMLTestRunner(output='C:\\Users\Duraze\\PycharmProjects\\pythonProject\\Reports',
+                                combine_reports=True, report_name="MyReport", report_title="Test Suites Report")
+        runner.run(smoke1)
+
+
+
 
 
     def tearDown(self):
-        self.driver.quit()
+        self.driver.close()
+
 
 
 if __name__=='__main__':
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='Reports'))
+    unittest.main()
