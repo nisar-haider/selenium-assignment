79e41459bc1d1cb901f5eb96cdb5b6f3267f59ce
DESKTOP-4483D1F\Nisar <nisar.haider454@gmail.com>
2021-10-12 16:48:56 +0500

    File: driver.py What: Added generic logic to load test suites Description: In this commit, I have added geenric logic to load and run all test suites and geenrate HTML report.

--- python/Tests/driver.py
+++ python/Tests/driver.py
@@ -1,3 +1,4 @@
+import self
 from selenium import webdriver
 import time
 import unittest
@@ -14,30 +15,51 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 #import Ebay
-from tintash import Tintash
-#from Ebay import Ebay
 import tintash
+#from tintash import Tintash1
+#from Ebay import Ebay
 import webAutomation
 from webAutomation import dragDrop
+from config import config1
 
-class driver1(unittest.TestCase):
+class driver1(config1):
 
-    def __init__(self, driver):
-        self.driver = driver
+    def callID(self):
+        return (self.id())
+
+    def run_all_suites(self):
+        # Create test suite.
+        test_suite = unittest.TestSuite()
+        # Load all test case class in current folder.
+        all_test_cases = unittest.defaultTestLoader.discover('.', '*.py')
+        # Loop the found test cases and add them into test suite.
+        for test_case in all_test_cases:
+            test_suite.addTests(test_case)
+
+            # Create HtmlTestRunner object and run the test suite.
+        test_runner = HtmlTestRunner.HTMLTestRunner(output='Reports', combine_reports=True)
+        test_runner.run(test_suite)
+
 
 
 
 
 
 if __name__=='__main__':
-    example_tests = TestLoader().loadTestsFromTestCase(Tintash)
-    example2_tests = TestLoader().loadTestsFromTestCase(dragDrop)
+    # conf = config()
+    # example_tests = TestLoader().loadTestsFromTestCase(Tintash)
+    # example2_tests = TestLoader().loadTestsFromTestCase(dragDrop)
+    #
+    # suite = TestSuite([example_tests, example2_tests])
+    #
+    # runner = HTMLTestRunner(output='C:\\Users\Duraze\\PycharmProjects\\pythonProject\\Reports', combine_reports=True, report_name="MyReport", report_title="Test Suites Report")
 
-    suite = TestSuite([example_tests, example2_tests])
+    # smoke_test.addTests([
+    #     unittest.defaultTestLoader.loadTestsFromTestCase(d.callID()),
+    #     #unittest.defaultTestLoader.loadTestsFromTestCase(webAutomation.dragDrop),
+    # ])
 
-    runner = HTMLTestRunner(output='example_suite')
-
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:\\Users\Duraze\\PycharmProjects\\pythonProject\\Reports', combine_reports=True))
+    driver1.run_all_suites(self=driver1)
 
 
 
