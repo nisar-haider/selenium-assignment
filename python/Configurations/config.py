0c5735d8ec2eebf022cd772c8321eda6d13204ad
DESKTOP-4483D1F\Nisar <nisar.haider454@gmail.com>
2021-10-14 15:11:26 +0500

    File: config.py What: Polishing of screen shots logic Description: In this commit, I have implemented logic of taking screen shots in desired folder.

--- python/Configurations/config.py
+++ python/Configurations/config.py
@@ -6,37 +6,30 @@ from HtmlTestRunner import HTMLTestRunner
 
 from unittest import TestLoader, TestSuite
 
-#import tintash
-#import webAutomation
-#import tintash
-#import Ebay
+
+# import tintash
+# import webAutomation
+# import tintash
+# import Ebay
 
 
 class config1(unittest.TestCase):
-    testname = ""
-    test_runner = ""
-    test_suite = ""
 
     # def setUpClass(cls):
     #     cls.test_runner = HtmlTestRunner.HTMLTestRunner(output="Reports")
     #     print(cls.test_runner)
 
-
     def setUp(self):
         self.driver = webdriver.Chrome("C:/Users/Duraze/PycharmProjects/pythonProject/chromedriver.exe")
         self.driver.maximize_window()
 
-
     def tearDown(self):
         for classmethod, error in self._outcome.errors:
             if error:
-                self.driver.get_screenshot_as_file("screenshot" + self.id() + ".png")
+                self.driver.get_screenshot_as_file('ScreenShots/' + self.id() + ".png")
 
         self.driver.close()
 
 
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
